195fc3432f21d6b53fce640c1f66a82c84871139
gentom <gento.m.developer@gmail.com>
2017-11-23 15:26:13 +0900

    test sensim.cluster

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import sys
-from sensim.sensim import Sensim
+from sensim.cluster.cluster import SensimCluster
 from matplotlib import pyplot as plt
 from scipy.cluster.hierarchy import dendrogram
 
-sensim = Sensim(sys.argv[1])
-ids = sensim.get_ids()
-result = sensim.ward()
+cluster = SensimCluster(sys.argv[1])
+ids = cluster.get_ids()
+result = cluster.ward()
 mod_ids = [id[-6:] for id in ids]
 r = dendrogram(result, p=100, truncate_mode='lastp', labels=mod_ids, leaf_rotation=90)
 print(r['leaves'])
